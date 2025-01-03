32b16b18a5830c64ba66e1693d11461ff314b51a
furaizi <furachannel16@gmail.com>
2025-01-03 16:18:34 +0200

    First commit

---
+++ src/main/java/org/example/restproject/entities/User.java
@@ -0,0 +1,69 @@
+package org.example.restproject.entities;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table
+@Data
+public class User {
+
+    @Id
+    private long id;
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+    @Column("isBanned")
+    private boolean isBanned;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean isBanned() {
+        return isBanned;
+    }
+
+    public void setBanned(boolean banned) {
+        isBanned = banned;
+    }
+}
